d7aecc45efaa340b4c4c8d0024b3837efc450a22
AlexSanchezBit-32 <alessq732@gmail.com>
2024-01-19 01:28:38 -0500

    corregida suma

--- Tools/AbelianGroup.py
+++ Tools/AbelianGroup.py
@@ -2,17 +2,14 @@ import cv2
 import numpy as np
 
 def Sum(A, B):
-    return min((A+B)%256, (-(A+B))%256)
+    # Ejemplo de operación Sum en dos arrays tridimensionales
+    return np.minimum((A + B) % 256, (-(A + B)) % 256)
+
 
 def ImgDif(A, B):
     height, width, channels = A.shape
     # Crear una matriz de ceros (imagen en negro)
-    image = np.zeros((height, width), dtype=np.uint8)
-
+    image =np.minimum((A + B) % 256, (-(A + B)) % 256)
     # Construir la imagen píxel a píxel
-    for y in range(height):
-        for x in range(width):
-            # Asignar un valor de intensidad (escala de grises) al píxel
-            image[y, x] = Sum(A[y,x], -B[y,x])
     cv2.imwrite("imSum.png", image)
     return cv2.imread("imSum.png")
--- Tools/StpCriteria.py
+++ Tools/StpCriteria.py
@@ -3,5 +3,7 @@ import numpy as np
 from Tools.AuxFunc import *
 
 def StpCrit(A, B, e):
-    return H(A, B) <= e
+    k =H(A, B)
+    print(k)
+    return k <= e
     
