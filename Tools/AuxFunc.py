302fc36ce99ffcab6d9e7947359ebb3d3b0ee048
AlexSanchezBit-32 <alessq732@gmail.com>
2024-01-19 01:40:49 -0500

    corregido error de la probabilidad

--- Tools/AuxFunc.py
+++ Tools/AuxFunc.py
@@ -23,14 +23,15 @@ def E(image):
 def Ep(image):
     # Convertir la imagen a escala de grises
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    linialgi = gray_image.flatten()
-    # Calcular el histograma de la imagen
-    hist, _ = np.histogram(linialgi, bins=256, range=[0,256])
-    
-    # Calcular la probabilidad de cada nivel de intensidad
+
+    # Calcular el histograma y las probabilidades de la imagen
+    hist, _ = np.histogram(gray_image.flatten(), bins=256, range=[0,256])
     prob = hist / np.sum(hist)
 
-    return np.sum(prob * linialgi)
+    # Calcular la esperanza (suma ponderada)
+    result = np.sum(prob * np.arange(256))
+    
+    return result
 
 
 def C(A, B):
